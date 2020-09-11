1690829c9d0fcb5c19d3c767a6b818e05bf76b70
kaiser2343 <kaiser.shaik@gmail.com>
2020-09-12 01:22:39 +0530

    Finished the bot lol

--- bot.py
+++ bot.py
@@ -1,19 +1,44 @@
-import discord
+from discord.ext.commands import Bot
+from discord import Game
+import requests
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+BOT_PREFIX = ('!')
+TOKEN = '<bot_token>'
+post_url = 'https://panel.freemc.host/api/client/servers/<server_id/power'
+get_url = 'https://panel.freemc.host/api/client/servers/<server_id>/utilization'
+header = {"Accept": "Application/vnd.pterodactyl.v1+json", 
+    "Content-Type": "application/json", 
+    "Authorization": "Bearer <pterodactyl_account_api_key>"
+}
 
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
+client = Bot(command_prefix=BOT_PREFIX)
 
-        if message.content.startswith('!hello'):
-            await message.channel.send('Hello {0.author.mention}'.format(message))
+@client.command(name="startserver", description="Starts Minecraft server", brief="Starts Minecraft server", pass_context=True)
+async def startserver(ctx):
+    params = {"signal" : "start"}
+    response = requests.post(post_url, json=params, headers = header)
+    if response.status_code == 204:
+        await ctx.send("Starting the server, "+ ctx.author.mention + ", enjoy!")
+    else:
+        await ctx.send("Hmmmm. There seems to be an error, "+ ctx.author.mention + ". Try again later?")
 
-client = MyClient()
-client.run('token')
+@client.command(name="stopserver", description="Stops Minecraft server", brief="Stops Minecraft server", pass_context=True)
+async def stopserver(ctx):
+    params = {"signal" : "stop"}
+    response = requests.post(post_url, json=params, headers = header)
+    if response.status_code == 204:
+        await ctx.send("Stopping the server, "+ ctx.author.mention + ", hope you had a great time!")
+    else:
+        await ctx.send("Hmmmm. There seems to be an error, "+ ctx.author.mention + ". Try again later?")
+
+@client.command(name="serverstatus", description="Check if server is running", brief="Check if server is running", pass_context=True)
+async def serverstatus(ctx):
+    response = requests.get(get_url, headers = header)
+    res = response.json()
+    if res["attributes"]["state"] == "on":
+        await ctx.send("Server is already on, "+ ctx.author.mention + ", get on there!")
+    elif res["attributes"]["state"] == "off":
+        await ctx.send("Welp, server's off "+ ctx.author.mention + ". Start it using the startserver command!")
+    else:
+        await ctx.send("Hmmmm. There seems to be an error, "+ ctx.author.mention + ". Try again later?")
+client.run(TOKEN)
