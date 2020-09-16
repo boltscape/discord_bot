8b4273dd881728de9723c56e4099d93ba8187bd9
Kaiser Shaik <kaiser.shaik@gmail.com>
2020-09-16 17:29:36 +0530

    Handled HTTP error

--- bot.py
+++ bot.py
@@ -38,15 +38,16 @@ async def restartserver(ctx):
         
 @client.command(name="status", description="Check if server is running", brief="Check if server is running", pass_context=True)
 async def serverstatus(ctx):
-    response = panel_client.client.get_server_utilization(srv_id)
-    if response['state'] == "on":
-        await ctx.send("Server is already on, "+ ctx.author.mention + ", get on there!")
-    elif response['state'] == "off":
-        await ctx.send("Welp, server's off "+ ctx.author.mention + ". Start it using the start command!")
-    elif response.status == 500:
+    try:
+        response = panel_client.client.get_server_utilization(srv_id)
+        if response['state'] == "on":
+            await ctx.send("Server is already on, "+ ctx.author.mention + ", get on there!")
+        elif response['state'] == "off":
+            await ctx.send("Welp, server's off "+ ctx.author.mention + ". Start it using the start command!")
+        else:
+            await ctx.send("Hmmmm. The server is either starting or stopping right now, "+ ctx.author.mention)
+    except requests.exceptions.HTTPError:
         await ctx.send("Shit. There was a server error, " + ctx.author.mention + ". Try again later?")
-    else:
-        await ctx.send("Hmmmm. The server is either starting or stopping right now, "+ ctx.author.mention)
 
 @client.command(name="thanks", description="Your way to thank the creeper xD", brief="Give thanks", pass_context=True)
 async def thanks(ctx):
